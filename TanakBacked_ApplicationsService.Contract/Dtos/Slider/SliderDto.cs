970d61133db938f2aaa3e3ab31690bab85ac8107
amirhosseinmakineh <amirhosseinmakineh1379@gmail.com>
2024-10-17 09:12:05 -0700

    getsliderimages

---
+++ TanakBacked_ApplicationsService.Contract/Dtos/Slider/SliderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TanakBackend_ApplicationsService.Contract.Dtos.Slider
+{
+    public class SliderDto
+    {
+        public long Id { get; set; }
+        public string ImageName { get; set; } = string.Empty;
+        public string Alt { get; set; } = string.Empty;
+    }
+}
---
+++ TanakBacked_ApplicationsService.Contract/IServices/ISliderService.cs
@@ -0,0 +1,9 @@
+﻿using TanakBackend_ApplicationsService.Contract.Dtos.Slider;
+
+namespace TanakBackend_ApplicationsService.Contract.IServices
+{
+    public interface ISliderService
+    {
+        ICollection<SliderDto> GetAll();
+    }
+}
